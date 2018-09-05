241ba0ffe5c0d22f558a9714d6bf69b3bac0049f
VadymBoguslavsky <axixe92@gmail.com>
2018-09-06 02:17:30 +0300

    template

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-  function myFunction() {
-   window.location.reload();
+function myFunction() {
+  window.location.reload();
+}
+function createList(param) {
+  var list = document.querySelector("#list")
+  var main = document.querySelector(".wrapper")
+  var newList = list.content.cloneNode(true);
+  newList.querySelector('.accordion').textContent = param.sectionName;
+  var text = newList.querySelector('.panel p');
+  var btn = newList.querySelector('.accordion');
+
+  function createText(el) {
+    const urlText = 'https://content.guardianapis.com/small-business-network/2016/feb/22/startup-of-the-year-competition-entry-pavillion-at-the-park?showblocks=body&api-key=7d20aee1-371a-4142-b225-eadb439e1a84'
+    fetch(urlText)
+      .then(
+        function (response) {
+          response.json().then(function (data) {
+            var result = data.response.content
+            el.textContent = result.apiUrl
+
+          });
+        }
+      )
   }
-  function createList(param) {
-   var button = document.createElement("button")
-   button.classList.add("accordion")
-   var main = document.querySelector(".wrapper")
-   main.appendChild(button)
-   var div = document.createElement("div")
-   div.classList.add("panel")
-   main.appendChild(div)
-   var p = document.createElement("p")
-   div.appendChild(p)
-   button.innerHTML = param.sectionName
-   fetch('http://content.guardianapis.com/small-business-network/2016/feb/22/startup-of-the-year-competition-entry-pavillion-at-the-park?showblocks=body&api-key=7d20aee1-371a-4142-b225-eadb439e1a84')
-    .then(
-     function (response) {
-      response.json().then(function (data) {
-       var result = data.response.content
-       p.innerHTML = result.apiUrl
-       activateAccordion(button);
-      });
-     }
-    )
-  }
-  fetch('https://content.guardianapis.com/search?api-key=7d20aee1-371a-4142-b225-eadb439e1a84')
-   .then(
+  createText(text)
+  main.appendChild(newList)
+  activateAccordion(btn);
+
+}
+const urlBtn = 'https://content.guardianapis.com/search?api-key=7d20aee1-371a-4142-b225-eadb439e1a84'
+fetch(urlBtn)
+  .then(
     function (response) {
-     response.json().then(function (data) {
-      var result = data.response.results
-      for (var key in result) {
-       createList(result[key])
-      }
-     });
+      response.json().then(function (data) {
+        var result = data.response.results
+        for (var key in result) {
+          createList(result[key])
+        }
+      });
     }
-   )
-  
-  function activateAccordion(acc) {
-   acc.addEventListener("click", function () {
+  )
+
+function activateAccordion(acc) {
+  acc.addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
-     panel.style.maxHeight = null;
+      panel.style.maxHeight = null;
     } else {
-     panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
-   });
-  }
+  });
+}
